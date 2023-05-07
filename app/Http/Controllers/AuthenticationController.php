074c75928bab654185d3f9222da62451c0929d1c
hossam <hossam0soliuman@gmail.com>
2023-05-07 03:27:24 +0200

    uploading project

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Traits\ApiResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticationController extends Controller
+{
+    use ApiResponse;
+
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users|max:255',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => bcrypt($validated['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return $this->customResponse(
+            [
+                'token' => $token,
+            ],
+            'Successfully registered.'
+        );
+    }
+
+    public function login(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if (!Auth::attempt($validated)) {
+            return $this->errorResponse('Unauthorized', 401);
+        }
+
+        $user = User::where('email', $validated['email'])->first();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return $this->customResponse(
+            [
+                'token' => $token,
+            ],
+            'Successfully logged in.'
+        );
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return $this->successResponse(
+            [],
+            'Successfully logged out.'
+        );
+    }
+}
